5d7e2040e888a946ba380bbf58f626f75f5cf9bb
sonupachauri <96345119+sonupachauri@users.noreply.github.com>
2025-03-11 18:42:44 +0000

    hooks

--- 02basicreact/src/main.jsx
+++ 02basicreact/src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
-function MyApp(){
-  return (
-    <h1>Custom MyApp</h1>
-  )
-}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <MyApp />
+    <App />
   </StrictMode>,
 )
